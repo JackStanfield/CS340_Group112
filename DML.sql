6685e3e0eb622f46b9b4e56db29bbc1eadcad061
austinalb <albrecau@oregonstate.edu>
2026-02-19 18:51:50 -0800

    Project Step 3 Code

--- DML.sql
+++ DML.sql
@@ -1,89 +1,80 @@
-------------------------
---Wizards
-------------------------
+-- Wizards: Get all Wizards with a self-join to show the mentor's name
+SELECT w1.wizardID, w1.wizardName, w1.beardLengthCm, w1.wizardAge, w2.wizardName AS mentorName 
+FROM Wizards w1 
+LEFT JOIN Wizards w2 ON w1.masterID = w2.wizardID;
 
---Get all Wizards
-SELECT wizardID, wizardName, beardLengthCm, masterID FROM Wizards;
---ORDER BY wizardID;
+-- Wizards: Create (masterID chosen via dropdown, can be NULL)
+INSERT INTO Wizards (wizardName, beardLengthCm, wizardAge, masterID)
+VALUES (:wizardNameInput, :beardLengthInput, :wizardAgeInput, :masterIDInput);
 
---CREATE Wizard (masterID choseen via dropdown, it may be NULL)
-INSERT INTO Wizards (wizardName, beardLengthCm, masterID)
-VALUES (:wizardNameInput, :beardLengthInput, :masterIDInput);
-
---Update Wizard
-UPDATE Wizards SET wizardName = :wizardNameInput, beardLengthCm = :beardLengthInput, masterID = :masterIDInput
+-- Wizards: Update
+UPDATE Wizards 
+SET wizardName = :wizardNameInput, beardLengthCm = :beardLengthInput, wizardAge = :wizardAgeInput, masterID = :masterIDInput
 WHERE wizardID = :wizardIDInput;
 
--- Delete wizard
+-- Wizards: Delete
 DELETE FROM Wizards WHERE wizardID = :wizardIDInput;
 
+-- Chronicles: Get all Chronicles with a JOIN to show the author's name
+SELECT chronicleID, chronicleTitle, publicationDate, description, Wizards.wizardName AS authorName 
+FROM Chronicles 
+LEFT JOIN Wizards ON Chronicles.wizardID = Wizards.wizardID;
 
-------------------------
---Chronicles
-------------------------
+-- Chronicles: Add
+INSERT INTO Chronicles (chronicleTitle, publicationDate, description, wizardID) 
+VALUES (:titleInput, :dateInput, :descInput, :wizardIDFromDropdown);
 
---Get all Chronicles
-SELECT chronicleID, chronicleTitle, wizardID FROM Chronicles;
-
--- Add chronicle
-INSERT INTO Chronicles (chronicleTitle, wizardID) 
-VALUES (:titleInput, :wizardIDFromDropdown);
-
---Update Chronicle
-UPDATE Chronicles SET chronicleTitle = :titleInput, wizardID = :wizardIDFromDropdownSET chronicleTitle = @chronicleTitleInput,
+-- Chronicles: Update (Fixed syntax error)
+UPDATE Chronicles 
+SET chronicleTitle = :titleInput, publicationDate = :dateInput, description = :descInput, wizardID = :wizardIDFromDropdown
 WHERE chronicleID = :chronicleIDInput;
 
--- Delete chronicle
+-- Chronicles: Delete
 DELETE FROM Chronicles WHERE chronicleID = :chronicleIDInput;
 
-------------------------
---Spells
-------------------------
-
--- Get all spells with their chronicle name
+-- Spells: Get all spells with their chronicle name
 SELECT Spells.spellID, Spells.spellName, Spells.castingInstruction, Chronicles.chronicleTitle 
 FROM Spells
 LEFT JOIN Chronicles ON Spells.chronicleID = Chronicles.chronicleID;
 
--- Add new spell
+-- Spells: Add new spell
 INSERT INTO Spells (spellName, castingInstruction, chronicleID) 
 VALUES (:spellNameInput, :instructionInput, :chronicleIDFromDropdown);
 
--- Update spell
-UPDATE Spells SET spellName = :spellNameInput, castingInstruction = :instructionInput, chronicleID = :chronicleIDFromDropdown 
+-- Spells: Update spell
+UPDATE Spells 
+SET spellName = :spellNameInput, castingInstruction = :instructionInput, chronicleID = :chronicleIDFromDropdown 
 WHERE spellID = :spellIDInput;
 
--- Delete spell
+-- Spells: Delete spell
 DELETE FROM Spells WHERE spellID = :spellIDInput;
 
-------------------------
---Categories
-------------------------
-
--- Get all categories
+-- Categories: Get all categories
 SELECT categoryID, categoryName FROM Categories;
 
--- Add category
+-- Categories: Add
 INSERT INTO Categories (categoryName) VALUES (:categoryNameInput);
 
--- Update category
+-- Categories: Update
 UPDATE Categories SET categoryName = :categoryNameInput WHERE categoryID = :categoryIDInput;
 
--- Delete category
+-- Categories: Delete
 DELETE FROM Categories WHERE categoryID = :categoryIDInput;
 
-------------------------
---SpellCategories (M:N Intersection)
-------------------------
-
--- Get all associations with names
-SELECT Spells.spellName, Categories.categoryName 
+-- SpellCategories (M:N Intersection): Get all associations with names
+SELECT SpellCategories.spellID, SpellCategories.categoryID, Spells.spellName, Categories.categoryName 
 FROM SpellCategories
 INNER JOIN Spells ON SpellCategories.spellID = Spells.spellID
 INNER JOIN Categories ON SpellCategories.categoryID = Categories.categoryID;
 
--- Add association
+-- SpellCategories: Add association
 INSERT INTO SpellCategories (spellID, categoryID) VALUES (:spellIDInput, :categoryIDInput);
 
--- Delete association (requires both IDs)
+-- SpellCategories: Delete association
 DELETE FROM SpellCategories WHERE spellID = :spellIDInput AND categoryID = :categoryIDInput;
+
+-- Dropdown Population Queries
+SELECT wizardID, wizardName FROM Wizards;
+SELECT chronicleID, chronicleTitle FROM Chronicles;
+SELECT categoryID, categoryName FROM Categories;
+SELECT spellID, spellName FROM Spells;
